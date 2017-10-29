48f0a5163e718e0a7034d9733506d9a7702c845f
Mitchel Hall <scubaphoto@gmail.com>
2017-10-28 22:05:31 -0700

    Feel I am very close to working code

--- app.js
+++ app.js
@@ -2,43 +2,123 @@
 
 var allImages = [];
 var imagesDisplayed = [];
-var x = '';
-var y = '';
-var z = '';
+var productVote = [];
+var a = 0;
+var b = 0;
+var c = 0;
+var x = allImages.length + 1;
+var y = allImages.length + 1;
+var z = -allImages.length + 1;
+var submitVote = 0;
 
-//creator function
-//create images 1-20 push to allImages
+console.log('Global variables declared');
+
+function CreateAllImages(name, filePath) {
+  this.name = name;
+  this.filePath = filePath;
+  allImages.push(this);
+}
+
+function populateAllImages() {
+  new CreateAllImages('Bag', 'img/bag.jpg');
+  new CreateAllImages('Banana', 'img/banana.jpg');
+  new CreateAllImages('Bathroom', 'img/bathroom.jpg');
+  new CreateAllImages('Boots', 'img/boots.jpg');
+  new CreateAllImages('Breakfast', 'img/breakfast.jpg');
+  new CreateAllImages('BubbleGum', 'img/bubblegum.jpg');
+  new CreateAllImages('Chair', 'img/chair.jpg');
+  new CreateAllImages('Cthulhu', 'img/cthulhu.jpg');
+  new CreateAllImages('Dog-Duck', 'img/dog-duck.jpg');
+  new CreateAllImages('Dragon', 'img/dragon.jpg');
+  new CreateAllImages('Pen', 'img/pen.jpg');
+  new CreateAllImages('PetSweep', 'img/pet-sweep.jpg');
+  new CreateAllImages('Scissors', 'img/scissors.jpg');
+  new CreateAllImages('Shark', 'img/shark.jpg');
+  new CreateAllImages('Sweep', 'img/sweep.png');
+  new CreateAllImages('Tauntaun', 'img/tauntaun.jpg');
+  new CreateAllImages('Unicorn', 'img/unicorn.jpg');
+  new CreateAllImages('Tentical', 'img/usb.gif');
+  new CreateAllImages('WaterCan', 'img/water-can.jpg');
+  new CreateAllImages('WineGlass', 'img/wine-glass.jpg');
+}
+
+function createRandomNumber() {
+  return Math.floor(Math.random() * (allImages.length));
+}
 
 function randomNumber() {
-var a = Math.floor(Math.random() * (allImages.length));
-while  (a === x || a === y || a === z);
-    a = Math.floor(Math.random() * (allImages.length));
-var b = Math.floor(Math.random() * (allImages.length));
-   while  (b === a || b === x || a === y || a === z);
-   b = Math.floor(Math.random() * (allImages.length));
-var c = Math.floor(Math.random() * (allImages.length));
-   while (c === b || c === a || c === x || c === y || c === z);
-   c = Math.floor(Math.random() * (allImages.length));
-x = a;
-y = b;
-z = c;
-imagesDisplayed.push(a,b,c);
-return (a,b,c,x,y,z);
-}
+  a = createRandomNumber();
+  while (a === x || a === y || a === z){
+    a = createRandomNumber();
+  }
+  b = createRandomNumber();
+  while (b === a || b === x || a === y || a === z) {
+    b = createRandomNumber();
+  }
+  c = createRandomNumber();
+  while (c === b || c === a || c === x || c === y || c === z){
+    c = createRandomNumber();
+  }
+
+  console.log(a + ' Value for A');
+  console.log(b + ' Value for B');
+  console.log(c + ' Value for C');
+  console.log(allImages);
+  x = a;
+  y = b;
+  z = c;
+  console.log(x + ' Value for X inside createRandomNumber');
+  console.log(y + ' Value for Y inside createRandomNumber');
+  console.log(z + ' Value for Z inside createRandomNumber');
+  // return (x,y,z);
+};
 
 function displayImages() {
+  populateAllImages();
   randomNumber();
-var imageOne = pull.allImages[a].filePath;
-var imageOneDescription = pull.allImages[a].description;
-var imageTwo = pull.allImages[b].filePath;
-var imagetwoDescription = pull.allImages[b].description;
-var imageThree = pull.allImages[c].filePath;
-var imageThreeDescription = pull.allImages[c].description;
-var imageOneEl = document.getElementById('imageOne');
-imageOneEl.src = imageOne;
-var imageTwoEl = imageTwo;
-var imageThreeEl = imageThree;
+  console.log(x + ' Outside value for X');
+  console.log(y + ' Outside value for Y');
+  console.log(z + ' Outside value for Z');
+  var imageOne = allImages[a].filePath;
+  imagesDisplayed.push(allImages[a].name);
+  var imageTwo = allImages[b].filePath;
+  imagesDisplayed.push(allImages[b].name);
+  var imageThree = allImages[c].filePath;
+  imagesDisplayed.push(allImages[c].name);
+
+  var imageOneEl = document.getElementById('imageOne');
+  imageOneEl.src = imageOne;
+  var imageTwoEl = document.getElementById('imageTwo');
+  imageTwoEl.src = imageTwo;
+  var imageThreeEl = document.getElementById('imageThree');
+  imageThreeEl.src = imageThree;
+  imagesDisplayed.push(imageOne,imageTwo,imageThree);
 }
-process user click.
-radio buttons x, y, or z
-Click equals x, y, or z
+displayImages();
+console.log(imagesDisplayed + ' Images Displayed');
+
+function clickOne() {
+  productVote.push(allImages[a].name);
+}
+function clickTwo() {
+  productVote.push(allImages[b].name);
+}
+function clickThree() {
+  productVote.push(allImages[c].name);
+}
+while (submitVote < 25) {
+  var imageOneClick = document.getElementById('imageOne');
+  var imageTwoClick = document.getElementById('imageTwo');
+  var imageThreeClick = document.getElementById('imageThree');
+  imageOneClick.addEventListener('click', clickOne) ;
+  imageTwoClick.addEventListener('click', clickTwo) ;
+  imageThreeClick.addEventListener('click', clickThree) ;
+
+  console.log(submitVote + ' Inside Function');
+  submitVote++;
+  displayImages();
+}
+
+// var imgEl = document.getElementById('goat-pic');
+//
+// imgEl.addEventListener('click', randomGoat);
