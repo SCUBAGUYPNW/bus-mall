13aa4eb6ab6c453ad4930b3e4df9cfefea2efc99
Mitchel Hall <scubaphoto@gmail.com>
2017-11-04 07:23:14 -0700

    commit before switching to Saturday branch

--- app.js
+++ app.js
@@ -64,59 +64,74 @@ function randomAllImages() {
   previouseForSecond = randomForSecond;
   previouseForThird = randomForThird;
 };
-
+debugger;
 function displayImages() {
-  randomAllImages();
-  var imageOne = allImages[randomForFirst].filePath;
-  imagesDisplayed.push(allImages[randomForFirst].name);
-  var imageTwo = allImages[randomForSecond].filePath;
-  imagesDisplayed.push(allImages[randomForSecond].name);
-  var imageThree = allImages[randomForThird].filePath;
-  imagesDisplayed.push(allImages[randomForThird].name);
-  // console.log(imagesDisplayed + 'images Displayed');
-
-  var imageOneEl = document.getElementById('imageOne');
-  imageOneEl.src = imageOne;
-  var imageTwoEl = document.getElementById('imageTwo');
-  imageTwoEl.src = imageTwo;
-  var imageThreeEl = document.getElementById('imageThree');
-  imageThreeEl.src = imageThree;
-//  imagesDisplayed.push(imageOne,imageTwo,imageThree);
+  if (productVote.length < 26) {
+    randomAllImages();
+    var imageOne = allImages[randomForFirst].filePath;
+    imagesDisplayed.push(allImages[randomForFirst].name);
+    var imageTwo = allImages[randomForSecond].filePath;
+    imagesDisplayed.push(allImages[randomForSecond].name);
+    var imageThree = allImages[randomForThird].filePath;
+    imagesDisplayed.push(allImages[randomForThird].name);
+    console.log(productVote.length + ' productVote length');
+    // console.log(imagesDisplayed + 'images Displayed');
+    var imageOneEl = document.getElementById('imageOne');
+    imageOneEl.src = imageOne;
+    var imageTwoEl = document.getElementById('imageTwo');
+    imageTwoEl.src = imageTwo;
+    var imageThreeEl = document.getElementById('imageThree');
+    imageThreeEl.src = imageThree;
+    processClicks();
+  } else {
+    removeClicks();
+  }
 }
+
+var imageOneClick = document.getElementById('imageOne');
+var imageTwoClick = document.getElementById('imageTwo');
+var imageThreeClick = document.getElementById('imageThree');
+
+function removeClicks() {
+  imageOneClick.removeEventListener('click', displayChart) ;
+  imageTwoClick.removeEventListener('click',displayChart) ;
+  imageThreeClick.removeEventListener('click',displayChart) ;
+  console.log('Remove Clicks Function');
+  displayChart();
+}
+
+function displayChart () {
+  console.log('This is how the chart will appear.');
+}
+
+//  imagesDisplayed.push(imageOne,imageTwo,imageThree);
+
 // console.log(imagesDisplayed + ' Images Displayed');
 
 displayImages();
-debugger;
-if (productVote.length < 25) {
-  console.log(productVote.length + ' productVote length inside of if.');
-  function clickOne() {
-    productVote.push(allImages[randomForFirst].name);
-    displayImages();
-    //console.log(productVote + ' click one, productVote');
-  }
-  function clickTwo() {
-    productVote.push(allImages[randomForSecond].name);
-    displayImages();
-    //console.log(productVote + ' click two, productVote');
-      }
-  function clickThree() {
-    productVote.push(allImages[randomForThird].name);
-    //console.log(productVote + ' click three, productVote');
-    displayImages();
-      }
+// debugger;
 
-    // event.preventDefault();
-    var imageOneClick = document.getElementById('imageOne');
-    var imageTwoClick = document.getElementById('imageTwo');
-    var imageThreeClick = document.getElementById('imageThree');
-    imageOneClick.addEventListener('click', clickOne) ;
-    imageTwoClick.addEventListener('click', clickTwo) ;
-    imageThreeClick.addEventListener('click', clickThree) ;
+function clickOne() {
+  productVote.push(allImages[randomForFirst].name);
+  displayImages();
+  //console.log(productVote + ' click one, productVote');
+}
+function clickTwo() {
+  productVote.push(allImages[randomForSecond].name);
+  displayImages();
+  //console.log(productVote + ' click two, productVote');
+}
+function clickThree() {
+  productVote.push(allImages[randomForThird].name);
+  //console.log(productVote + ' click three, productVote');
+  displayImages();
 }
 
-  if (productVote.length > 24) {
-    imageOneClick.removeEventListener('click', clickOne) ;
-    imageTwoClick.removeEventListener('click', clickTwo) ;
-    imageThreeClick.removeEventListener('click', clickThree) ;
-    console.log(productVote.length + ' ProductVote > 24');
+// event.preventDefault();
+
+function processClicks() {
+  imageOneClick.addEventListener('click', clickOne) ;
+  imageTwoClick.addEventListener('click', clickTwo) ;
+  imageThreeClick.addEventListener('click', clickThree) ;
+  console.log(productVote.length + ' ProductVote > 24');
 }
